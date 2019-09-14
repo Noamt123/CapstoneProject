d1e37b8c3a6c4d9ef71571c3b4d080253cde3844
Noam Tuchman <51915849+Noamt123@users.noreply.github.com>
2019-09-14 17:30:32 -0400

    Update push_docker.sh

--- push_docker.sh
+++ push_docker.sh
@@ -2,6 +2,6 @@ docker build --tag=capstone .
 
 dockerpath=beartuchman/capstone
 
-docker tag capstone ${dockerpath}:newester3
+docker tag capstone ${dockerpath}:newester3.1
 
-docker push ${dockerpath}:newester3
+docker push ${dockerpath}:newester3.1
